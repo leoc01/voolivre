51f285580714c9bf275b50afff6717b21dd2ebe3
leoc01 <leonardocampetti@gmail.com>
2023-08-07 11:19:15 +0100

    Inclui o propósito

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Home() {
   return (
     <div>
-      <h1>A história do Voo Livre.</h1>
+      <h1>A história do Voo Livre, até agora.</h1>
       <Link href="/sobre">Sobre o blog</Link>
     </div>
   );
--- pages/sobre.js
+++ pages/sobre.js
@@ -4,6 +4,11 @@ function Sobre() {
   return (
     <div>
       <h1>Sobre o blog.</h1>
+      <p>
+        {" "}
+        Este blog tem um único propósito: Tornar o esporte do voo livre cada vez
+        mais acessível para todos os que tem interesse em conhecer e voar
+      </p>
       <Link href="/">Página inicial</Link>
     </div>
   );
