aa6db767910190d0d27b480420af43f4a3a23202
leoc01 <leonardocampetti@gmail.com>
2023-10-31 17:00:41 -0300

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function Status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function Status(request, response) {
+  const result = await database.query("SELECT 5+2+1 as pimba;");
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média!" });
 }
 
